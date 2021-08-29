6c502c273cfaeae59c037a247b95d48986c2678f
Jéssica Rech <jssrech@gmail.com>
2021-08-29 04:08:09 -0300

    Configura porta do Heroku

--- server.js
+++ server.js
@@ -18,6 +18,4 @@ app.use(express.json());
 app.use(cors());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
-app.listen('5000', () => {
-    console.log('conectado na porta 5000');
-});
+app.listen(process.env.PORT || '5000');
